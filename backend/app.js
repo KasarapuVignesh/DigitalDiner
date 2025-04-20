03287dcc5cd180280fa1e39aed8e33e3a85b22cc
KasarapuVignesh <134696325+KasarapuVignesh@users.noreply.github.com>
2025-04-20 15:15:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+  origin:["https://digital-diner.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+}));
 app.use(express.json());
 
 // Connect to DBs
